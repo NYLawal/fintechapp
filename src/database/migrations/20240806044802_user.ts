698a545e712d28955cfe9664f1a8716a6649c2a1
Nurat Lawal <nurahawwal@yahoo.com>
2024-08-18 01:16:37 +0100

    dropTable command added to down function

--- src/database/migrations/20240806044802_user.ts
+++ src/database/migrations/20240806044802_user.ts
@@ -2,15 +2,19 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-    return knex.schema
+    await knex.schema
     .createTable('users', function (table) {
-      table.increments('id');
-      table.string('first_name', 255).notNullable();
-      table.string('last_name', 255).notNullable();
+      table.increments('id').primary();
+      table.string('first_name').notNullable();
+      table.string('last_name').notNullable();
+      table.string('email').notNullable().unique();
+      table.string('password').notNullable();
+      table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
     })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users');
 }
 
